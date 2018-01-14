edf48d5a8a3018afffd122b7357426104db6ad9f
amosbastian <amos_bastian@hotmail.com>
2018-01-14 00:12:01 +0000

    Update stream_blockchain.py

--- part_2/stream_blockchain.py
+++ part_2/stream_blockchain.py
@@ -5,23 +5,23 @@ import json
 import datetime
 
 def converter(object_):
-	if isinstance(object_, datetime.datetime):
-		return object_.__str__()
+    if isinstance(object_, datetime.datetime):
+        return object_.__str__()
 
 def stream_blockchain():
-	blockchain = Blockchain()
-	stream = map(Post, blockchain.stream(filter_by=['comment']))
-	while True:
-		try:
-			for post in stream:
-				tags = post["tags"]
-				if post.is_main_post() and "utopian-io" in tags:
-					author = post["author"]
-					title = post["title"]
-					print("{} posted {}".format(author, title))
-		except Exception as error:
-			print(repr(error))
-			continue
+    blockchain = Blockchain()
+    stream = map(Post, blockchain.stream(filter_by=['comment']))
+    while True:
+        try:
+            for post in stream:
+                tags = post["tags"]
+                if post.is_main_post() and "utopian-io" in tags:
+                    author = post["author"]
+                    title = post["title"]
+                    print("{} posted {}".format(author, title))
+        except Exception as error:
+            print(repr(error))
+            continue
 
 if __name__ == '__main__':
-	stream_blockchain()
+    stream_blockchain()
