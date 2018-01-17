c0168b7caede00a3eb919a2ada3f7a2be124fc42
amosbastian <amos_bastian@hotmail.com>
2018-01-17 15:56:46 +0000

    Finish memo.py

--- part_8/memo.py
+++ part_8/memo.py
@@ -1,1 +1,27 @@
-import steem as Steem
+from steem import Steem
+from steem.blockchain import Blockchain
+import json
+import datetime
+
+def converter(object_):
+    if isinstance(object_, datetime.datetime):
+        return object_.__str__()
+
+def run():
+    steem = Steem()
+    blockchain = Blockchain()
+    stream = blockchain.stream(filter_by=["transfer"])
+
+    username = "amosbastian"
+
+    while True:
+        try:
+            for transfer in stream:
+                if transfer["to"] == username:
+                    url, permlink = transfer["memo"].split("@")
+                    if "https://steemit.com/" in url:
+                        steem.vote(f"@{permlink}", 100)
+        except Exception as error:
+            print(repr(error))
+            continue
+                
