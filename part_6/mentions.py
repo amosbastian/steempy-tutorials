79cb492100ab0aae1be34785a2278e497d95511a
amosbastian <amos_bastian@hotmail.com>
2018-01-17 14:51:09 +0000

    Update mentions.py

--- part_6/mentions.py
+++ part_6/mentions.py
@@ -5,25 +5,29 @@ import json
 import datetime
 import re
 
-steem = Steem()
-blockchain = Blockchain()
-stream = map(Post, blockchain.stream(filter_by=["comment"]))
-
 def converter(object_):
     if isinstance(object_, datetime.datetime):
         return object_.__str__()
 
-REGEX = "(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)"
-username = "amosbastian"
+def run():
+    steem = Steem()
+    blockchain = Blockchain()
+    stream = map(Post, blockchain.stream(filter_by=["comment"]))
 
-while True:
-    try:
-        for post in stream:
-            mentions = re.findall(REGEX, post["body"])
-            if username in mentions:
-                print(f"Replying to {post['author']}")
-                post.reply(f"Hey @{post['author']} thanks for mentioning me!")
+    REGEX = "(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)"
+    username = "amosbastian"
 
-    except Exception as error:
-        print(repr(error))
-        continue
+    while True:
+        try:
+            for post in stream:
+                mentions = re.findall(REGEX, post["body"])
+                if username in mentions:
+                    print(f"Replying to {post['author']}")
+                    post.reply(f"Hey @{post['author']}, you mentioned me?")
+
+        except Exception as error:
+            print(repr(error))
+            continue
+
+if __name__ == '__main__':
+    run()
